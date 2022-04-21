6f946ab33e9a1b78613b4619ecf87b68d70d3869
Muhammed yasir Keles <mukeles@k1m13s03.42kocaeli.com.tr>
2022-04-21 14:33:27 +0300

    fdkj

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukeles <mukeles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 14:40:07 by mukeles           #+#    #+#             */
+/*   Updated: 2022/03/02 14:40:10 by mukeles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_new_left_str(char *left_str);
+char	*ft_search_n_str(int fd, char *left_str);
+char	*ft_get_one_line(char *left_str);
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukeles <mukeles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 14:40:17 by mukeles           #+#    #+#             */
+/*   Updated: 2022/03/02 14:40:19 by mukeles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*s3;
+	int		index;
+	int		index2;
+
+	index = -1;
+	index2 = 0;
+	if (!s2)
+		return (NULL);
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char));
+		s1[0] = '\0';
+	}
+	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (s3 == NULL)
+		return (NULL);
+	while (s1[++index] != '\0')
+		s3[index] = s1[index];
+	while (s2[index2] != '\0')
+		s3[index++] = s2[index2++];
+	s3[index] = '\0';
+	free(s1);
+	return (s3);
+}
